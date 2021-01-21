2d446bf5bbed6233bffb9726ba2dbdf5252316ca
Ganius <temahomganius@gmail.com>
2021-01-21 15:36:06 +0000

    dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+ @extends('layout.index')
 
-        <title>Garage</title>
+@section('titre')
+<h1>Bienvenue au Gestion de garage</h1>
+@endsection
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    </head>
-    <body class="antialiased">
-        <h1>Bienvenue au Gestion de garage</h1>
-       
-    </body>
-</html>
+<h1>Le contenu ici...</h1>
+@endsection
+        
+     
