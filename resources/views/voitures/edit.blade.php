1cb14c964ebb77174db534869d45dfc5c8b5e02a
zizdevweb <ziz17fall@gmail.com>
2021-01-22 15:51:01 +0000

    'crud_voiture_2'

---
+++ resources/views/voitures/edit.blade.php
@@ -0,0 +1,31 @@
+@extends('layout.index')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Modifier Voiture</h2>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>warning</strong>Veuillez vérifier vos saisies<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li> {{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>   
+    @endif
+
+    <form action="{{ route('voitures.update', $voiture->id) }}" method="POST">
+        @csrf
+        @method('PATCH')
+
+        @include('voitures._partials._form');
+
+    </form>
+    
+@endsection
