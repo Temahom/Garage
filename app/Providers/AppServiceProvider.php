3be7a5415d2d64efee953bbc8cb5cd569a678567
Ganius <temahomganius@gmail.com>
2021-01-22 15:31:46 +0000

    modif medoune

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-<<<<<<< HEAD
         //
-=======
->>>>>>> f8bed79f8415bf4296effc6926112795690fab16
     }
 }
