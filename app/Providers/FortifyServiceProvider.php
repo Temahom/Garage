3226f051992d85088ab4337054fc0ae69cb9cce0
Ganius <temahomganius@gmail.com>
2021-01-28 16:22:29 +0000

    rectification

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -1,13 +1,17 @@
 <?php
 
 namespace App\Providers;
-
 use App\Actions\Fortify\CreateNewUser;
+use App\Models\User;
 use App\Actions\Fortify\ResetUserPassword;
+use App\Actions\Fortify\UpdateUserPassword;
+use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
-
+use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
 class FortifyServiceProvider extends ServiceProvider
 {
     /**
@@ -27,8 +31,6 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
-
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
@@ -50,9 +52,6 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::registerView(fn () => view('auth.register'));
 
         
-        Fortify::loginView(function (){
-            return view('auth.login');
-        });
         Fortify::authenticateUsing(function (Request $request) {
             $user = User::where('email', $request->email)->first();
     
@@ -73,12 +72,4 @@ class FortifyServiceProvider extends ServiceProvider
 
        
     }
-
-        //RateLimiter::for('login', function (Request $request) {
-        //    return Limit::perMinute(5)->by($request->email.$request->ip());
-        //});
-
-        //RateLimiter::for('two-factor', function (Request $request) {
-        //    return Limit::perMinute(5)->by($request->session()->get('login.id'));
-        //});
 }
