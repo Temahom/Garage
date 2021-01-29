cf1a50a0e5b5870e31ea772a155fe63f2cd40f3d
Moustapha <moustapha.mg28@gmail.com>
2021-01-29 11:27:42 +0000

    r

--- app/Models/Produit.php
+++ app/Models/Produit.php
@@ -8,11 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Produit extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-    protected $guarded=[]; 
-=======
 
     protected $guarded=[];
 
->>>>>>> f99ad20085352091323a31dc8c8a36a7a4b51cc3
 }
