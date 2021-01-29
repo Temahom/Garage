5d9fddc4ebc34b35a2fed3a73f3f2c9a1b518859
Moustapha <moustapha.mg28@gmail.com>
2021-01-29 13:36:24 +0000

    r

--- resources/views/diagnostics/create.blade.php
+++ resources/views/diagnostics/create.blade.php
@@ -8,6 +8,17 @@
             </div>
         </div>
     </div>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Attention</strong>Veuillez vérifier vos saisies<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li> {{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>   
+    @endif
+
 
     <form action="{{ route('voitures.interventions.diagnostics.store',['voiture' => $voiture->id, 'intervention' => $intervention->id]) }}" method="POST">
         {{ csrf_field() }}
