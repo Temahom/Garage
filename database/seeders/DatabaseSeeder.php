4b3a9ac20869e3010eda11a0898ec8fb53bd9873
Moustapha <moustapha.mg28@gmail.com>
2021-01-31 22:47:21 +0000

    r

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,38 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::create([
+            "name"=>"Moustapha",
+            "email"=>"moustapha@gmail.com",
+            "role_id"=>(1),
+            "password"=>bcrypt(12345678)
+        ]);
+
+        \App\Models\User::create([
+            "name"=>"Mohamet",
+            "email"=>"mohamet@gmail.com",
+            "role_id"=>(2),
+            "password"=>bcrypt(12345678)
+        ]);
+
+        \App\Models\User::create([
+            "name"=>"Medoune",
+            "email"=>"medoune@gmail.com",
+            "role_id"=>(3),
+            "password"=>bcrypt(12345678)
+        ]);
+
+        \App\Models\Role::create([
+            'id'=>(1),
+            'role'=>'Admin'
+        ]);
+        \App\Models\Role::create([
+            'id'=>(2),
+            'role'=>'Raf'
+        ]);
+        \App\Models\Role::create([
+            'id'=>(3),
+            'role'=>'Manager'
+        ]);
     }
 }
