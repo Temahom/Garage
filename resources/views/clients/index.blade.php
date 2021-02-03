a6ebdf5170dbb7d3ab7b83ab27fccf43eb80be53
moussathiam <moussathiam80@gmail.com>
2021-02-03 14:28:35 +0000

    aziz

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -7,7 +7,7 @@
 				<h2>Tous les Clients</h2>
 			</div>
 			<div class="pull-right">
-				<a class="btn btn-primary" href="{{route('clients.create')}}">Enregistrer un Client</a>
+				<a class="btn btn-secondary" href="{{route('clients.create')}}">Enregistrer un Client</a>
 			</div>
 		</div>
 	</div>
@@ -21,29 +21,28 @@
 	@endif
 
 	<div class="row">
-	<div class="col-md-12 margin-tb">
-	<table class="table table-striped table-hover col-md-12">
-		<thead class="thead-dark">
+	<div class="col-md-12 col-lg-11">
+	<table class="table table-striped table-hover ">
+		<thead class="" style="background-color: #4656E9;">
 			<tr>
-				<th>Nom</th>
-				<th>Prénoms</th>
-				<th>Genre</th>
-				<th>Entreprise</th>
-				<th>Téléphone</th>
-				<th>Action</th>
+				<th style="color: white;">Nom</th>
+				<th style="color: white;">Prénoms</th>
+				<th style="color: white;">Genre</th>
+				<th style="color: white;">Entreprise</th>
+				<th style="color: white;">Téléphone</th>
+				<th style="color: white;">Action</th>
 			</tr>
 		</thead>
 		<tbody>
 		@foreach ($clients as $client)
 		
-			<tr onclick="showClient({{ $client->id }})" style="cursor: pointer;">>
-				<td>{{ $client->nom }}</td>
-				<td>{{ $client->prenom }}</td>
-				<td style="text-transform: capitalize;">{{ $client->genre }}</td>
-				<td>{{ $client->entreprise }}</td>
-				<td>{{ $client->telephone }}</td>
+			<tr>
+				<td onclick="showClient({{ $client->id }})" style="cursor: pointer; text-transform: capitalize;">{{ $client->nom }}</td>
+				<td onclick="showClient({{ $client->id }})" style="cursor: pointer; text-transform: capitalize;">{{ $client->prenom }}</td>
+				<td onclick="showClient({{ $client->id }})" style="cursor: pointer; text-transform: capitalize;">{{ $client->genre }}</td>
+				<td onclick="showClient({{ $client->id }})" style="cursor: pointer; text-transform: capitalize;">{{ $client->entreprise }}</td>
+				<td onclick="showClient({{ $client->id }})" style="cursor: pointer; text-transform: capitalize;">{{ $client->telephone }}</td>
 				<td>
-					<a class="btn btn-success p-0 pr-2 pl-2" href="{{route('clients.show',$client->id)}}"><i class="fas fa-eye"></i></a>
 					<a class="btn btn-primary p-0 pr-2 pl-2" href="{{ route('clients.edit',$client->id)}}"><i class="fas fa-edit"></i></a>
 					<button type="button" class="btn btn-danger p-0 pr-2 pl-2" data-toggle="modal" data-target="#exampleModal{{ $client->id }}">
 						<i class="fas fa-trash"></i>
