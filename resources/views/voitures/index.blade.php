3dfb3dd8ad3d309d58311d33e01066ee0e6b61e7
moussathiam <moussathiam80@gmail.com>
2021-02-03 22:06:47 +0000

    ibou

--- resources/views/voitures/index.blade.php
+++ resources/views/voitures/index.blade.php
@@ -25,37 +25,35 @@
     
         @endif
     <div class="row">
-    <div class="col-lg-12 margin-tb">
+    <div class="col-lg-11 col-md-12">
         <table class="table table-striped table-hover col-md-12">
-            <thead class="thead-dark">
+            <thead class="" style="background-color: #4656E9;">
         <tr>
-            <th>Matricule</th>
-            <th>Marque</th>
-            <th>Model</th>
-            <th>Annee</th>
-            <th>Carburant</th>
-            <th>Puissance</th>
-            <th>Enregistré par</th>
-            <th width='275px'>Action</th>
+            <th style="color: white;">Matricule</th>
+            <th style="color: white;">Marque</th>
+            <th style="color: white;">Model</th>
+            <th style="color: white;">Annee</th>
+            <th style="color: white;">Carburant</th>
+            <th style="color: white;">Puissance</th>
+            <th style="color: white;">Enregistré par</th>
+            <th style="color: white;">Action</th>
         </tr>
             </thead>
         @foreach ($voitures as $voiture)
         <tr>
-            <td>{{ $voiture->matricule}}</td>
-            <td>{{ $voiture->marque}}</td>
-            <td>{{ $voiture->model}}</td>
-            <td>{{ $voiture->annee}}</td>
-            <td>{{ $voiture->carburant}}</td>
-            <td>{{ $voiture->puissance}}</td>
+            <td onclick="showVoiture({{ $voiture->id }})" style="cursor: pointer; text-transform: capitalize;">{{ $voiture->matricule}}</td>
+            <td onclick="showVoiture({{ $voiture->id }})" style="cursor: pointer; text-transform: capitalize;">{{ $voiture->marque}}</td>
+            <td onclick="showVoiture({{ $voiture->id }})" style="cursor: pointer; text-transform: capitalize;">{{ $voiture->model}}</td>
+            <td onclick="showVoiture({{ $voiture->id }})" style="cursor: pointer; text-transform: capitalize;">{{ $voiture->annee}}</td>
+            <td onclick="showVoiture({{ $voiture->id }})" style="cursor: pointer; text-transform: capitalize;">{{ $voiture->carburant}}</td>
+            <td onclick="showVoiture({{ $voiture->id }})" style="cursor: pointer; text-transform: capitalize;">{{ $voiture->puissance}}</td>
             <td> <div> {{$voiture->user()->first()->name}} <span class="badge badge-secondary capitalize">{{ $voiture->user()->first()->role()->first()->role}}</span></div> 
                 
             <td>
             {{-- <form action="{{ route('voitures.destroy',$voiture->id) }}" method="POST">    --}}
-                    <a class="btn btn-info" href="{{ route('voitures.show',$voiture->id) }}"><i class="fas fa-eye mr-2"></i></a>    
-                
-                    <a class="btn btn-primary" href="{{ route('voitures.edit',$voiture->id) }}"><i class="fas fa-edit mr-2"></i></a>
-                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal{{ $voiture->id }}">
-                        <i class="fas fa-trash mr-2"></i>
+                    <a class="btn btn-primary  p-0 pr-2 pl-2" href="{{ route('voitures.edit',$voiture->id) }}"><i class="fas fa-edit"></i></a>
+                    <button type="button" class="btn btn-danger  p-0 pr-2 pl-2" data-toggle="modal" data-target="#exampleModal{{ $voiture->id }}">
+                        <i class="fas fa-trash"></i>
                     </button> 
                     <div class="modal fade" id="exampleModal{{ $voiture->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 						<div class="modal-dialog" role="document">
@@ -87,5 +85,14 @@
 		</div>
 	</div>
 
+    
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+
+	<script>
+		function showVoiture(id)
+		{
+			window.location = 'voitures/' + id ;
+		}
+	</script>
 
 @endsection
