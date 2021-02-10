4f721e8b2b78efd8a0666c1cfefed27c0a6f896e
Ganius <temahomganius@gmail.com>
2021-02-10 23:46:24 +0000

    to deploy

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+    protected $fillable =['id'];
     public function users(){
         return $this->hasMany(User::class);
     }
