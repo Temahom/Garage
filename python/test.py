8f67bdd9ac5b695a6b2f7b013216e1fb38e2d7e2
Ganius <temahomganius@gmail.com>
2021-02-12 13:51:47 +0000

    seeder pour la liste des produits

--- python/test.py
+++ python/test.py
@@ -3,7 +3,7 @@ f = open("VoitureSeed.txt", "a")
 r=requests.get('http://127.0.0.1:8000/api/listes')
 voitures=r.json()
 for v in voitures:
-    v2='\App\Models\Liste::create(["marques"=>"'+v["marques"]+'","lemodel"=>"'+v["lemodel"]+'","lannee"=>"'+v["lemodel"]+'","lecarburant"=>"'+v["lecarburant"]+'","lapuissance"=>'+v["lapuissance"]+']);'
+    v2='\App\Models\Liste::create(["marques"=>"'+v["marques"]+'","lemodel"=>"'+v["lemodel"]+'","lannee"=>"'+v["lannee"]+'","lecarburant"=>"'+v["lecarburant"]+'","lapuissance"=>'+v["lapuissance"]+']);'
     f.write("\n"+v2)
     print(v2)
 
