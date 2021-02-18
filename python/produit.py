a8390a5b1e2bddb3a9cfce44282ea52e8425fdd1
Ganius <temahomganius@gmail.com>
2021-02-18 12:12:06 +0000

    tous les seeders

--- python/produit.py
+++ python/produit.py
@@ -1,7 +1,8 @@
 import requests
-f = open("ProduitSeed.txt", "a")
+f = open("../database/seeders/ProduitSeed.php", "a")
 r=requests.get('http://127.0.0.1:8000/api/listesp')
 voitures=r.json()
+f.write("<?php \n\n")
 for v in voitures:
     v2='\App\Models\listeproduit::create(["categorie"=>"'+v["categorie"]+'","produit"=>"'+v["produit"]+'","prix1"=>'+str(v["prix1"])+']);'
     f.write("\n"+v2)
