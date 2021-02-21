6b35e1863a5cbe72772a21a1b7a93d686441756f
aidarakarara <smedoune32@gmail.co>
2021-02-21 16:18:04 +0000

    by medoune produits select input

--- resources/views/produits/edit.blade.php
+++ resources/views/produits/edit.blade.php
@@ -12,52 +12,51 @@ $listes=listeproduit::select('categorie')->orderBy('categorie','asc')->distinct(
                 <h2>Modifiez Le Produit</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('produits.index') }}" title="Go back"><span style="font-size:15px;">&#129060;</span> Retour </a>
+                <a class="btn btn-primary" href="{{ route('produits.index') }}" title="Go back"><span style="font-size:15px;">&#129060;</span> Retour </a><br>
             </div>
         </div>
     </div>
 
-
+    <br>
     <form action="{{ route('produits.update', $produit->id) }}" method="POST">
         @csrf
         @method('PUT')
 
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="col-xs-6 col-sm-6 col-md-6">
                 <div class="form-group">
                     <strong>Categorie :</strong>
-                    <select name="categorie" id="categorie" class="custom-select form-control  @error('categorie') is-invalid @enderror">
+                    <select name="categorie" id="categorie" class="custom-select form-control  @error('categorie') is-invalid @enderror" onchange="change();">
 						<option value="{{ $produit->categorie }}">{{ $produit->categorie }}</option>
 							@foreach ($listes as $liste)
 								<option value="{{$liste->categorie}}" {{ old('categorie') == ($liste->categorie) ? 'selected' : '' }}>{{$liste->categorie}}</option>
 							@endforeach
 					</select>
+                    <input name="categorie" type="text" class="custom-select form-control" id="inpuTxt1" style="display:none;" value="{{ $produit->categorie }}"/>
                     <div class="invalid-feedback">
                         @if($errors->has('categorie'))
                         {{ $errors->first('categorie') }}
                         @endif
                     </div>		
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                <strong>Nom du produit :</strong>
-                <select name="produit" id="leproduit" class="custom-select form-control  @error('produit') is-invalid @enderror">
-                    <option value="{{ $produit->produit }}" {{ old('produit') == ($produit->produit) ? 'selected' : '' }}>{{ $produit->produit }}</option>
-                </select>
-                <input name="produit" type="text" value="{{ $produit->produit }}" class="custom-select form-control" id="inpuTxt2" style="display:none;" placeholder="Mettre le nom du produit"/>
-                <div class="invalid-feedback">
-                    @if($errors->has('produit'))
-                    {{ $errors->first('produit') }}
-                    @endif
-                </div>
-                </div>		
+                    <strong>Nom du produit :</strong>
+                    <select name="produit" id="leproduit" class="custom-select form-control  @error('produit') is-invalid @enderror" onchange="change();">
+                        <option value="{{ $produit->produit }}" {{ old('produit') == ($produit->produit) ? 'selected' : '' }}>{{ $produit->produit }}</option>
+                    </select>
+                    <input name="produit" type="text" value="{{ $produit->produit }}" class="custom-select form-control" id="inpuTxt2" style="display:none;" placeholder="Mettre le nom du produit"/>
+                    <div class="invalid-feedback">
+                        @if($errors->has('produit'))
+                        {{ $errors->first('produit') }}
+                        @endif
+                    </div>
+                    </div>		
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="col-xs-6 col-sm-6 col-md-6">
                 <div class="form-group">
                     <strong>Prix Unitaire :</strong>
-                    <select name="prix1" id="leprix" class="form-control @error('prix1') is-invalid @enderror" >     
-                    <option value="{{ $produit->prix1 }}" {{ old('prix1') == ($produit->prix1) ? 'selected' : '' }}>{{ $produit->prix1 }}</option>s
+                    <select name="prix1" id="leprix" class="form-control @error('prix1') is-invalid @enderror" onchange="change();">     
+                        <option value="{{ $produit->prix1 }}" {{ old('prix1') == ($produit->prix1) ? 'selected' : '' }}>{{ $produit->prix1 }}</option>s
                     </select>
                     <input name="prix1" type="number" value="{{ $produit->prix1 }}" class="custom-select form-control" id="inpuTxt3" style="display:none;" autocomplete="off" placeholder="Mettre le prix du produit"/>
                     <div class="invalid-feedback">
@@ -66,20 +65,18 @@ $listes=listeproduit::select('categorie')->orderBy('categorie','asc')->distinct(
                         @endif
                     </div>
                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Quantité:</strong>
-                    <input type="number" name="qte" value="{{ $produit->qte }}" class="form-control @error('qte') is-invalid @enderror" placeholder="Entrer la quantite" value="{{ old('qte') }}">
-                    <div class="invalid-feedback">
-                        @if($errors->has('qte'))
-                        {{ $errors->first('qte') }}
-                        @endif
-                    </div>
+               <div class="form-group">
+                <strong>Quantité:</strong>
+                <input type="number" name="qte" value="{{ $produit->qte }}" class="form-control @error('qte') is-invalid @enderror" placeholder="Entrer la quantite" value="{{ old('qte') }}">
+                <div class="invalid-feedback">
+                    @if($errors->has('qte'))
+                    {{ $errors->first('qte') }}
+                    @endif
                 </div>
             </div>
+            </div>
             <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Commander</button>
+                <br><button type="submit" class="btn btn-primary">Commander</button>
             </div>
         </div>
 
@@ -146,7 +143,7 @@ $listes=listeproduit::select('categorie')->orderBy('categorie','asc')->distinct(
             var input6 = document.getElementById("leproduit");
             var input7 = document.getElementById("categorie");
             
-            if(val=="autres")
+            if(val=="Autres")
             {
                 input1.style.display="none";
                 input2.style.display="block";
