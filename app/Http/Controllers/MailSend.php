2e4628eecc62933cc976b2235939936cb5829e8d
Ganius <temahomganius@gmail.com>
2021-02-22 15:18:23 +0000

    sms

--- app/Http/Controllers/MailSend.php
+++ app/Http/Controllers/MailSend.php
@@ -9,10 +9,13 @@ class MailSend extends Controller
     public function mailsend()
     {
         $details = [
-            'title' => 'Dev-MaLin',
-            'body' => 'It s very funs'
+            'title' => 'Renumération plus les jours fériés',
+            "body" => "Bonjour chers Développeurs de Mediapex \n Nous avons le plaisir de vous annoncer le verssement de votre prime qui sera disponible le Vendredi 26 de ce mois .<br> Veillez agréer cher tous l\'expression de nos salutations distinguées ."
         ];
-
+        \Mail::to('medoune.sene@mediapex.pro')->send(new SendMail($details));
+        \Mail::to('moustapha.gueye@mediapex.pro')->send(new SendMail($details));
+        \Mail::to('aziz.fall@mediapex.pro')->send(new SendMail($details));
+        \Mail::to('moussa.thiam@mediapex.pro')->send(new SendMail($details));
         \Mail::to('fatoubibi96@gmail.com')->send(new SendMail($details));
         return view('emails.thanks');
     }
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\InterventionController;
 use App\Http\Controllers\CommandeController;
 use App\Http\Controllers\ActorController;
 use App\Http\Controllers\MailSend;
+use App\Http\Controllers\SmsController;
 
 use Illuminate\Support\Facades\App;
 
@@ -31,6 +32,7 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
 Route::get('send-mail',[MailSend::class,'mailsend']);
+Route::get('send-message',[SmsController::class,'sendMessage']);
 Route::get('Pdf', function () {
    
    $commandes= \App\Models\Commande::all();
