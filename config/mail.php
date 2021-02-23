2cecc1fef55fd682e74fe13227f8c67d81267368
Ganius <temahomganius@gmail.com>
2021-02-23 12:32:14 +0000

    xzse

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,7 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'auth_mode' => null,
+            'auth_mode' => true,
         ],
 
         'ses' => [
@@ -62,13 +62,7 @@ return [
             'path' => '/usr/sbin/sendmail -bs',
         ],
 
-        'stream' => [
-            'ssl' => [
-                'allow_self_signed' => true,
-                'verify_peer' => false,
-                'verify_peer_name' => false,
-            ],
-        ],
+       
 
         'log' => [
             'transport' => 'log',
@@ -79,7 +73,13 @@ return [
             'transport' => 'array',
         ],
     ],
-
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ],
     /*
     |--------------------------------------------------------------------------
     | Global "From" Address
