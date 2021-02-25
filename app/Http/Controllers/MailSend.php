89a72ed387123c12dbf157907b14ce9069b0e0d8
Ganius <temahomganius@gmail.com>
2021-02-25 12:21:01 +0000

    attche

--- app/Http/Controllers/MailSend.php
+++ app/Http/Controllers/MailSend.php
@@ -8,17 +8,21 @@ class MailSend extends Controller
 {
     public function mailsend()
     {
+        $commandes= \App\Models\Commande::all();
+   
+        $pdf = PDF::loadView('Pdf.pdf',["commandes"=>$commandes]);    
+        
         $details = [
             'title' => 'Renumération plus les jours fériés',
-            "body" => "Bonjour chers Développeurs de Mediapex \n Nous avons le plaisir de vous annoncer le verssement de votre prime qui sera disponible le Vendredi 26 de ce mois .<br> Veillez agréer cher tous l\'expression de nos salutations distinguées ."
+            "body" => "Bonjour chers Développeurs"
         ];
-        \Mail::to('medoune.sene@mediapex.pro')->send(new SendMail($details));
-        \Mail::to('moustapha.gueye@mediapex.pro')->send(new SendMail($details));
-        \Mail::to('aziz.fall@mediapex.pro')->send(new SendMail($details));
-        \Mail::to('moussa.thiam@mediapex.pro')->send(new SendMail($details));
-        \Mail::to('fatoubibi96@gmail.com')->send(new SendMail($details));
-        \Mail::to('hildedokou@gmail.com')->send(new SendMail($details));
-        \Mail::to('diattamohamet30@gmail.com')->send(new SendMail($details));
+        // \Mail::to('medoune.sene@mediapex.pro')->send(new SendMail($details));
+        // \Mail::to('moustapha.gueye@mediapex.pro')->send(new SendMail($details));
+        // \Mail::to('aziz.fall@mediapex.pro')->send(new SendMail($details));
+        // \Mail::to('moussa.thiam@mediapex.pro')->send(new SendMail($details));
+        // \Mail::to('fatoubibi96@gmail.com')->send(new SendMail($details));
+        \Mail::to('hildedokou@gmail.com')->send(new SendMail($details))->attachData($pdf->output(), "invoice.pdf");
+        \Mail::to('diattamohamet30@gmail.com')->send(new SendMail($details))->attachData($pdf->output(), "invoice.pdf");
         return view('emails.thanks');
     }
 }
