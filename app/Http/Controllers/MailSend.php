dbd8aaf74692b0339a66f40df422a2c00b9d9bfe
Ganius <temahomganius@gmail.com>
2021-02-25 12:32:04 +0000

    attche

--- app/Http/Controllers/MailSend.php
+++ app/Http/Controllers/MailSend.php
@@ -15,15 +15,17 @@ class MailSend extends Controller
         
         $details = [
             'title' => 'Renumération plus les jours fériés',
-            "body" => "Bonjour chers Développeurs"
+            "body" => "Bonjour chers Développeurs",
+            "file"=>$pdf->output()
+            
         ];
         // \Mail::to('medoune.sene@mediapex.pro')->send(new SendMail($details));
         // \Mail::to('moustapha.gueye@mediapex.pro')->send(new SendMail($details));
         // \Mail::to('aziz.fall@mediapex.pro')->send(new SendMail($details));
         // \Mail::to('moussa.thiam@mediapex.pro')->send(new SendMail($details));
         // \Mail::to('fatoubibi96@gmail.com')->send(new SendMail($details));
-        \Mail::to('hildedokou@gmail.com')->send(new SendMail($details))->attachData($pdf->output(), "invoice.pdf");
-        \Mail::to('diattamohamet30@gmail.com')->send(new SendMail($details))->attachData($pdf->output(), "invoice.pdf");
+        \Mail::to('hildedokou@gmail.com')->send(new SendMail($details));
+        \Mail::to('diattamohamet30@gmail.com')->send(new SendMail($details));
         return view('emails.thanks');
     }
 }
