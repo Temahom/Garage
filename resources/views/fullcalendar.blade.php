7e9c4b3f1fd59ab79e23f12c60f1343429f140a4
aidarakarara <smedoune32@gmail.co>
2021-03-01 13:01:34 +0000

    by medoune

--- resources/views/fullcalendar.blade.php
+++ resources/views/fullcalendar.blade.php
@@ -4,8 +4,6 @@
 <head>
     <title>Laravel Fullcalender Tutorial Tutorial - ItSolutionStuff.com</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-  
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
@@ -17,7 +15,7 @@
 <body>
   
 <div class="container">
-    <h1>Laravel FullCalender Tutorial Example - ItSolutionStuff.com</h1>
+    <h1>Calendrier des événements</h1>
     <div id='calendar'></div>
 </div>
    
