5fab23d5b9931df161ebbb09857d93442c7f2216
Ganius <temahomganius@gmail.com>
2021-03-02 13:17:58 +0000

    bonjoyr

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -184,9 +184,9 @@ use Carbon\Carbon;
     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
         <div class="card border-3 border-top border-top-primary">
             <div class="card-body">
-                <h5 class="text-muted">Visitor</h5>
+                <h5 class="text-muted">En Stock</h5>
                 <div class="metric-value d-inline-block">
-                    <h1 class="mb-1">13000</h1>
+                    <h1 class="mb-1">{{ $produit_en_stock}}</h1>
                 </div>
                 <div class="metric-label d-inline-block float-right text-success font-weight-bold">
                     <span class="icon-circle-small icon-box-xs text-success bg-success-light"><i class="fa fa-fw fa-arrow-up"></i></span><span class="ml-1">5%</span>
