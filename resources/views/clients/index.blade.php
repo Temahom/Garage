2865f3aa4500ea4350fcf85785bc0d0125e67de6
Ganius <temahomganius@gmail.com>
2021-03-02 11:46:13 +0000

    tableau des clients

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layout.index')
 
 @section('content')
+<link rel="stylesheet" type="text/css" href="/assets/vendor/datatables/css/dataTables.bootstrap4.css">
+<link rel="stylesheet" type="text/css" href="/assets/vendor/datatables/css/buttons.bootstrap4.css">
+<link rel="stylesheet" type="text/css" href="/assets/vendor/datatables/css/select.bootstrap4.css">
+<link rel="stylesheet" type="text/css" href="/assets/vendor/datatables/css/fixedHeader.bootstrap4.css">
 	<div class="row">
 		<div class="col-lg-12 margin-tb">
 			<div class="pull-left">
@@ -91,12 +95,12 @@
 									</tbody>
 									<tfoot>
 										<tr>
-											<th style="color: white;">Prénoms</th>
-											<th style="color: white;">Genre</th>
-											<th style="color: white;">Entreprise</th>
-											<th style="color: white;">Téléphone</th>
-											<th style="color: white;">Email</th>
-											<th style="color: white;">Action</th>
+											<th >Prénoms</th>
+											<th >Genre</th>
+											<th>Entreprise</th>
+											<th >Téléphone</th>
+											<th >Email</th>
+											<th >Action</th>
 										</tr>
 									</tfoot>
 								</table>
@@ -141,6 +145,19 @@
                 classe.forEach(tr => tbody.appendChild(tr));
             }));
 </script>
-      
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="/assets/vendor/datatables/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+<script src="/assets/vendor/datatables/js/buttons.bootstrap4.min.js"></script>
+<script src="/assets/vendor/datatables/js/data-table.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.colVis.min.js"></script>
+<script src="https://cdn.datatables.net/rowgroup/1.0.4/js/dataTables.rowGroup.min.js"></script>
+<script src="https://cdn.datatables.net/select/1.2.7/js/dataTables.select.min.js"></script>
+<script src="https://cdn.datatables.net/fixedheader/3.1.5/js/dataTables.fixedHeader.min.js"></script>
 @endsection
    
