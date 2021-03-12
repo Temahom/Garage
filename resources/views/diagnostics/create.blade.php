94320455463f30ad0d148668b0e2a4e73c049644
Moustapha <moustapha.mg28@gmail.com>
2021-03-12 12:05:28 +0000

    r

--- resources/views/diagnostics/create.blade.php
+++ resources/views/diagnostics/create.blade.php
@@ -266,6 +266,27 @@
         return divDefaut;
     }
 
+    /*DEBUT gestion des doublons*/
+        const doublon=()=>{
+                    const selects = document.querySelectorAll('.custom-select');
+                    selects.forEach((elem) => {
+                        elem.addEventListener('change', (event) => {
+                            let values = Array.from(selects).map(select => select.value);
+                            for (let select of selects) {
+                            select.querySelectorAll('option').forEach((option) => {
+                                let value = option.value;
+                                if (value &&  value !== select.value && values.includes(value)) {
+                                    option.disabled = true;
+                                } else {
+                                    option.disabled = false;
+                                }
+                            });
+                        }
+                    });
+                });
+            }
+            doublon();
+        /*FIN gestion des doublons*/
 
     $("#add-btn").click(function(){
         div = getDiv(i);
@@ -337,27 +358,5 @@
         $('#formDiag').submit();
     }
     /* FIN CONTROL DIAGNOSTQUE*/
-
-    /*DEBUT gestion des doublons*/
-        const doublon=()=>{
-            const selects = document.querySelectorAll('.custom-select');
-            selects.forEach((elem) => {
-                elem.addEventListener('change', (event) => {
-                    let values = Array.from(selects).map(select => select.value);
-                    for (let select of selects) {
-                    select.querySelectorAll('option').forEach((option) => {
-                        let value = option.value;
-                        if (value &&  value !== select.value && values.includes(value)) {
-                            option.disabled = true;
-                        } else {
-                            option.disabled = false;
-                        }
-                    });
-                }
-            });
-        });
-    }
-    doublon();
-    /*FIN gestion des doublons*/
 </script>
 @endsection
