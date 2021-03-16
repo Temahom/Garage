4da374c6f98e48c970a3e5496d2762301c357d91
Temahom <temahomganius@gmail.com>
2021-03-16 11:10:24 +0000

    Bizzard

--- resources/views/interventions/index.blade.php
+++ resources/views/interventions/index.blade.php
@@ -164,7 +164,7 @@ body::before {
               <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
                  <div class="row">
                
-                      <table id="example4" class="table table-striped table-bordered" style="width:100%">
+                      <table class="table table-striped table-bordered" style="width:100%">
                       <thead>
                         <tr style="background-image: linear-gradient( to top,#2b2a34, #0E0C28); text-align: center">
                           <th scope="col" style= "color:#ffffff" > N<sup>o</sup></th>
