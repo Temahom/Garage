53071b91ebeb7c776c0af03b98a7c702a7575e5e
aidarakarara <smedoune32@gmail.co>
2021-03-22 11:45:57 +0000

    responsive

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -176,10 +176,7 @@
             <br>
             <div class="menu-list">
                 <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="d-xl-none d-lg-none" href="#">Clients</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+                    
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav flex-column">
                             <li class="nav-divider" style="font-size: 25px">
