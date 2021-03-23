18935445d5c41650a9f51a1ed6951edcfdfefa94
aidarakarara <smedoune32@gmail.co>
2021-03-23 11:04:31 +0000

    responsive

--- resources/views/actors/show.blade.php
+++ resources/views/actors/show.blade.php
@@ -69,7 +69,7 @@ use Carbon\Carbon;
 
                 <div class="row ml-1" style="justify-content:center;">
                   <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10">
-                    <div  class="card d-flex justify-content-center mr-2" style="width: 18rem; justify-content: center; text-align: center; cursor: pointer;">
+                    <div  class="card d-flex justify-content-center mr-2" style="margin-left: 30%; width: 18rem; justify-content: center; text-align: center; cursor: pointer;">
                         @if(isset($user->image))
                           <img class="d-flex justify-content-center " style="align-self:center;width: 100px ; height: 100px; border-radius: 50%;" src="{{asset('images/'.$user->image)}}" alt="Card image cap">
                         @else
