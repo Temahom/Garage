6023793129fa605530617a3868b9c80e02ac91c2
aidarakarara <smedoune32@gmail.co>
2021-03-24 10:14:23 +0000

    responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -208,15 +208,9 @@ use Carbon\Carbon;
         <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
             <div class="card border-3 border-top border-top-primary">
                 <div class="card-body">
-<<<<<<< HEAD
                     <h5 class="text-muted">Les Ventes de ce Mois-ci</h5>
                     <div class="metric-value d-inline-block">
                         <h1 class="mb-1">{{ $produit_en_stock}}</h1>
-=======
-                    <h5 style="color: #fcfcfc;" class="text-muted">Les Ventes de ce Mois-ci</h5>
-                    <div class="metric-value d-inline-block" >
-                        <h1  style="color: #fcfcfc;"  class="mb-1">{{ $produit_en_stock}}</h1>
->>>>>>> acc1171483aed26ffa7e4d87c4698ac81e56ea94
                     </div>
                     <div class="metric-label d-inline-block float-right text-success font-weight-bold">
                         <span class="icon-circle-small icon-box-xs text-success bg-success-light"><i class="fa fa-fw fa-arrow-up"></i></span><span class="ml-1">5%</span>
