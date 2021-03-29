7a735248abfda296d3eb0880f5a9182623219e49
aidarakarara <smedoune32@gmail.co>
2021-03-29 14:25:58 +0000

    responsive

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -1,11 +1,7 @@
 @extends('layout.index')
 @section('content')
 
-<style>
-	.row{
-		overflow-x: auto;
-	}
-</style>
+
 
     <div class="row ml-1">
         <div class="col-md-5 col-sm-5 py-1"  style="box-shadow: 0px 0px 2px rgb(145, 135, 135); background-color: #fafafa;">
