70953f34f41e7100257814c1131117c644aed82e
Temahom <temahomganius@gmail.com>
2021-03-31 15:47:46 +0100

    les courbes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -522,140 +522,18 @@ $jour_ci = Carbon::now()->day;
         integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw=="
         crossorigin="anonymous"></script>
     <script src="assets/libs/js/clock.js"></script>
+   
     <!-- morris js -->
-    <script src="assets/vendor/charts/morris-bundle/raphael.min.js"></script>
-    <script src="assets/vendor/charts/morris-bundle/morris.js"></script>
-    <script src="assets/vendor/charts/morris-bundle/morrisjs.html"></script>
+    <script src="/assets/vendor/charts/morris-bundle/raphael.min.js"></script>
+    <script src="/assets/vendor/charts/morris-bundle/morris.js"></script>
+    <script src="/assets/vendor/charts/morris-bundle/morrisjs.html" type="text/html"></script>
+    <script src="assets/libs/js/les_courbes.js"></script>
 
-    <script>
-        function show(page) {
-            window.location = page;
-        }
-
-    </script>
+   
 
 
     <script>
-        var moris;
-        var pourcent_ca;
-        var pourcent_cai;
-        var lab=[];
-        var prix_ca=[];
-        var prix_cai=[];
-        var int1=new Intl.NumberFormat();
-        $.ajax({
-            url:'/api/chart',
-            type:'GET',
-            success: function(data) {
-              
-                moris=data[0].chiffre;
-                $('#paye').html(int1.format(moris.data.CA)+" <sup>F CFA</sup>");
-                $('#impaye').html(int1.format(moris.data.CAI)+" <sup>F CFA</sup>");
-
-                pourcent_ca=moris.data.CA*100/(moris.data.CAI+moris.data.CA);
-                pourcent_cai=moris.data.CAI*100/(moris.data.CAI+moris.data.CA);
-                data.forEach(c=>{
-                    lab.push(c.chiffre.mois);
-                    prix_ca.push(c.chiffre.data.CA);
-                    prix_cai.push(c.chiffre.data.CAI);
-                   //console.log(prix_cai);
-                })
-                
-            }
-        })
-        // Chiffre affaire de ce mois
-        setTimeout(()=>{
-            Morris.Donut({
-            element: 'morris_gross',
-
-            data: [{
-                    value: Math.round(pourcent_ca),
-                    label: 'Payé'
-                },
-                {
-                    value: Math.round(pourcent_cai),
-                    label: 'Impayé'
-                }
-
-            ],
-
-            labelColor: '#5969ff',
-
-            colors: [
-                '#5969ff',
-                'rgba(255, 64, 123,.8)'
-
-            ],
-
-            formatter: function(x) {
-                return x + "%"
-            },
-            resize: true
-
-        });
-        
-        },500)
-        //clearInterval(time)
-       // console.log(moris);
-        // FIN Chiffre affaire de ce mois
-
-
-        // Chiffre affaire par mois
-        var ctx = document.getElementById("chartjs_balance_bar").getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-
-            data: {
-                labels: lab,
-                datasets: [{
-                    label: 'Chiffre d\'afaire',
-                    data: prix_ca,
-                    backgroundColor: "rgba(89, 105, 255,.8)",
-                    borderColor: "rgba(89, 105, 255,1)",
-                    borderWidth: 2
-
-                }, {
-                    label: 'Detes ',
-                    data: prix_cai,
-                    backgroundColor: "rgba(255, 64, 123,.8)",
-                    borderColor: "rgba(255, 64, 123,1)",
-                    borderWidth: 2
-
-
-                }]
-
-            },
-            options: {
-                legend: {
-                    display: true,
-
-                    position: 'bottom',
-
-                    labels: {
-                        fontColor: '#71748d',
-                        fontFamily: 'Circular Std Book',
-                        fontSize: 14,
-                    }
-                },
-
-                scales: {
-                    xAxes: [{
-                        ticks: {
-                            fontSize: 14,
-                            fontFamily: 'Circular Std Book',
-                            fontColor: '#71748d',
-                        }
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            fontSize: 14,
-                            fontFamily: 'Circular Std Book',
-                            fontColor: '#71748d',
-                        }
-                    }]
-                }
-            }
-        });
+       
         // FIN Chiffre affaire par mois
 
 
@@ -725,5 +603,11 @@ $jour_ci = Carbon::now()->day;
         });
 
        
+    </script>
+     <script>
+        function show(page) {
+            window.location = page;
+        }
+
     </script>
 @endsection
