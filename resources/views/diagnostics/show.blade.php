63a435e5bf9d1b999a182c661461285b958072be
aidarakarara <smedoune32@gmail.co>
2021-03-31 11:25:40 +0000

    responsive

--- resources/views/diagnostics/show.blade.php
+++ resources/views/diagnostics/show.blade.php
@@ -13,65 +13,70 @@
         </div>
     </div>
 
-      <div class="row" style="margin-top: 30px">
-        <div class="col-xs-12 col-sm-12 col-md-12 row">
-          <div class="col-2">
-            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-              <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Resume</a>
-              <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Details</a>
+    <br>
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 row">
+              <div class="col-xs-1 col-sm-1 col-md-1">
+                  <table class="table table-borderless">
+                    <thead>
+                      <tr class="table-primary">
+                        <th scope="col" style="text-align: center"><i class="fas fa-user"></i>N°</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td style="text-align: center">{{$diagnostic->id}}</td>
+                      </tr>
+                    </tbody>
+                  </table>               
+              </div>
+              <div class="col-xs-4 col-sm-4 col-md-4">
+                  <table class="table table-borderless">
+                    <thead>
+                      <tr class="table-primary">
+                        <th scope="col" style="text-align: center"><i class="fas fa-search"></i> Constat</th>
+                        <th scope="col" style="text-align: center"><i class="far fa-calendar-alt"></i> Date d'examination</th>
+                        <th scope="col" style="text-align: center"><i class="fas fa-user"></i> Chef d'opération</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th style="text-align: center" scope="row">{{$diagnostic->constat}}</th>
+                        <td style="text-align: center">{{$diagnostic->created_at}}</td>
+                        <td style="text-align: center">{{$diagnostic->intervention()->first()->user()->first()->name}}</td>
+                      </tr>
+                    </tbody>
+                  </table>               
+              </div>
+              <div class="col-xs-7 col-sm-7 col-md-7">
+                <table class="table table-borderless">
+                  <thead>
+                    <tr class="table-primary">
+                      <th scope="col" style="text-align: center"><i class="fas fa-key"></i> </th>
+                      <th scope="col" style="text-align: center"><i class="fas fa-link"></i> Localisation</th>
+                      <th scope="col" style="text-align: center"><i class="fas fa-info-circle"></i> Description</th>
+                      <th scope="col" style="text-align: center">Etat</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      @foreach ($diagnostic->defauts()->get() as $defaut)
+                    <tr>
+                      <td scope="row" style="text-align: center">{{$defaut->code}}</td>
+                      <td style="text-align: center">{{$defaut->localisation}}</td>
+                      <td style="text-align: center">{{$defaut->description}}</td>
+                      @if($defaut->etat==1)
+                      <td style="text-align: center" title='Bon Etat'><i class="fas fa-thumbs-up " style="color:green"></i></td>
+                      @elseif($defaut->etat==2)
+                      <td style="text-align: center" title="A Reparer"><i class="fas fa-minus-circle"></i></td>
+                      @else
+                      <td style="text-align: center" title='defectueux'><i class="fas fa-ban" style="color:red"></i></td>
+                      @endif
+                    </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-          <div class="col-10">
-            <div class="tab-content" id="v-pills-tabContent">
-              <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                  <table class="table table-borderless">
-                      <thead>
-                        <tr class="table-primary">
-                          <th scope="col" ><i class="fas fa-search"></i> Constat</th>
-                          <th scope="col"><i class="far fa-calendar-alt"></i> Date d'examination</th>
-                          <th scope="col"><i class="fas fa-user"></i> Chef d'opération</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <th scope="row">{{$diagnostic->constat}}</th>
-                          <td>{{$diagnostic->created_at}}</td>
-                          <td>{{$diagnostic->intervention()->first()->user()->first()->name}}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-              </div>
-              <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-                  <table class="table table-borderless">
-                      <thead>
-                        <tr class="table-primary">
-                          <th scope="col"><i class="fas fa-key"></i> </th>
-                          <th scope="col"><i class="fas fa-link"></i> Localisation</th>
-                          <th scope="col"><i class="fas fa-info-circle"></i> Description</th>
-                          <th scope="col">Etat</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                          @foreach ($diagnostic->defauts()->get() as $defaut)
-                        <tr>
-                          <td scope="row">{{$defaut->code}}</td>
-                          <td>{{$defaut->localisation}}</td>
-                          <td>{{$defaut->description}}</td>
-                          @if($defaut->etat==1)
-                          <td title='Bon Etat'><i class="fas fa-thumbs-up " style="color:green"></i></td>
-                          @elseif($defaut->etat==2)
-                          <td title="A Reparer"><i class="fas fa-minus-circle"></i></td>
-                          @else
-                          <td title='defectueux'><i class="fas fa-ban" style="color:red"></i></td>
-                          @endif
-                        </tr>
-                        @endforeach
-                      </tbody>
-                    </table>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     
 @endsection
