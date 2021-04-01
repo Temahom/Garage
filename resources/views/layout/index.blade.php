fa3959ba53ab71ba37e3c8854b4ba2aaf9fba5f6
aidarakarara <smedoune32@gmail.co>
2021-04-01 12:53:21 +0000

    responsive

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -27,7 +27,13 @@
 </head>
 <style>
 
-    
+    @media screen and (max-width: 767px) {
+  .row {
+          overflow-x: auto !important;
+  }
+
+  }  
+
     .dashboard-main-wrapper{
         background-color: white;
     }
