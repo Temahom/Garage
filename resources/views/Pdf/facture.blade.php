fa15bf363a3d471f8d6bb623c4e66c8549def9d8
Moustapha <moustapha.mg28@gmail.com>
2021-04-06 15:11:51 +0000

    r

--- resources/views/Pdf/facture.blade.php
+++ resources/views/Pdf/facture.blade.php
@@ -401,7 +401,7 @@
                     </strong>
                 </p>
             </td>
-            <td style="width:300px;border:none;border-bottom:solid black 1px;background:#eaead3;height:33.0px;">
+            <td style="border:none;border-bottom:solid black 1px;background:#eaead3;height:33.0px;">
                 <p style="line-height:normal;font-size:15px;font-family:montserrat;text-align:center;">
                     <strong>
                         <span style="font-size:16px;font-family:montserrat;">{{ number_format($prix_total+$prixHT+$cout, 0, ',', ' ') }}</span>
