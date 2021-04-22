47b3877ee05ad476a6a01594528e1f6ed90e0e59
Moustapha <moustapha.mg28@gmail.com>
2021-04-22 14:54:49 +0000

    r

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -308,7 +308,7 @@
                             <li class="nav-item "   style="padding-top: 5px">
                                 <a class="nav-link active" href="/gestion_stock" aria-expanded="false" ><i class="fa fa-university" aria-hidden="true"></i>Gestion Stock<span class="badge badge-success"></span></a>
                             </li>
-                            @endcan
+                            
                         </ul>
                     </div>
                 </nav>
