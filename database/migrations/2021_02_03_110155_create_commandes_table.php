20b980bacd717caffd92b5921285f7491fbb97aa
FatouMbaye <fatoubibi96@gmail.com>
2021-04-29 12:15:37 +0200

    ?

---
+++ database/migrations/2021_02_03_110155_create_commandes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->id();
+        /*    $table->foreignId('devi_id'); */
+          /*  $table->longText('catProduit');
+            $table->longText('produit_id'); */
+          //  $table->float('qteProduit');
+            $table->integer('etat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commandes');
+    }
+}
---
+++ database/migrations/2021_04_20_120859_create_commande_produits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandeProduitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commande_produits', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('commande_id');
+            $table->unsignedBigInteger('produit_id');
+            $table->integer('quantite');
+            $table->timestamps();// 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commande_produits');
+       
+    }
+}
